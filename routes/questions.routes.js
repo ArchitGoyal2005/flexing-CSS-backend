9f7e41a3c43e96953aecc712b57707cbd5fc890c
sunnnyy04 <sunnyshukla4900@gmail.com>
2024-07-31 19:56:04 +0530

    question routes done

---
+++ routes/questions.routes.js
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import {Question, Question} from "../models/questionModel.js"
+import { User } from "../models/userModel.js"
+import catchAsync from "../utils/catchAsync.js";
+import AppError from "../utils/AppError.js";
+import { ApiResponses } from "../utils/ApiReasponses.js";
+
+
+const router =Router()
+const getQuestions = catchAsync(async (req, res) => {
+    const user = await User.findById(req.user?.id);
+    if (!user) {
+        throw new AppError("user not found",404);
+    }
+
+    const questions = user.questions;
+
+    if (!questions || questions.length === 0) {
+        throw new AppError('No questions found', 400);
+    } else {
+        const questionsData = await Promise.all(
+            questions.map(async questionId => {
+                const question = await Question.findOne({ questionId });
+                if (!question) {
+                    throw new AppError(`Question with ID ${questionId} not found`, 404);
+                }
+                return question;
+            })
+        );
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                questions: questionsData
+            }
+        });
+    }
+});
+
+router.route("/:userId/questions").get(getQuestions)
--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,4 +1,4 @@
-export default (fn) => {
+export default catchAsync=(fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
   };
